eea3b930eab9ab4ca3ef30834fe098567bb67219
Arthur Pushkin <arthur.pushkin@flatsoft.com>
2011-12-09 23:39:09 +0400

    About triangle done

--- koans-day1/triangle.rb
+++ koans-day1/triangle.rb
@@ -14,5 +14,18 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  ab = a == b
+  bc = b == c
+  ca = c == a
+  if ab && bc && ca
+    :equilateral
+  elsif ab || bc ||ca
+    :isosceles
+  else
+    if a + b < c || b + c < a || c + a < b
+      :not_a_triangle
+    else
+      :scalene
+    end
+  end
 end
